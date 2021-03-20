122185ffedd39c93d7a396cc72b935e68f85216e
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-20 21:19:46 +0000

    checkbox functionality
    Checkbox now checks and text now lines through

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -3,36 +3,47 @@ import 'package:flutter/material.dart';
 class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFF757575),
+    return SingleChildScrollView(
       child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30.0),
-            topRight: Radius.circular(30.0),
+        height: MediaQuery.of(context).size.height,
+        color: Color(0xFF757575),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
           ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Text(
-                'Add Task',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.lightBlueAccent,
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Add Task',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              TextField(),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.lightBlueAccent,
+                TextField(
+                  autofocus: true,
+                  textAlign: TextAlign.center,
+                  textCapitalization: TextCapitalization.sentences,
                 ),
-                child: Text('Add'),
-              ),
-            ],
+                SizedBox(height: 5.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lightBlueAccent,
+                  ),
+                  child: Text('Add'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
