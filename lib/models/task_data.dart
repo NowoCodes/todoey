3edbd9314c323adfd79616bd13eacd122eb8edf0
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-30 23:28:57 +0100

    Update task_data.dart

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:todoey/models/task.dart';
+import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
   List<Task> _tasks = [
@@ -8,9 +9,7 @@ class TaskData extends ChangeNotifier {
     Task(name: 'Buy Bread'),
   ];
 
-  List<Task> get tasks {
-    return _tasks;
-  }
+  UnmodifiableListView<Task> get tasks => UnmodifiableListView(_tasks);
 
   int get taskCount => _tasks.length;
 
